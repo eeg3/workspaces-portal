984be2938e367fdafa597572cab356f9567abc72
Earl Gay <earl@eeg3.net>
2017-12-19 23:44:17 -0500

    reboot and rebuild functionality

--- lambda/workspaces-control/workspaces-control.js
+++ lambda/workspaces-control/workspaces-control.js
@@ -145,6 +145,160 @@ exports.handler = (event, context, callback) => {
             }
         });
 
+    } else if (action == "rebuild") {
+        console.log("Trying to find desktop owned by: " + event.requestContext.authorizer.claims.email);
+
+        var describeWorkspacesParams = [];
+
+        workspaces.describeWorkspaces(describeWorkspacesParams, function (err, data) {
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+            } else {
+                //console.log(data);
+                //console.log(data.Workspaces[0].WorkspaceId);
+
+                for (var i = 0; i < data.Workspaces.length; i++) {
+                    //console.log("Desktop[" + i + "]: " + data.Workspaces[i].WorkspaceId + " is owned by: " + data.Workspaces[i].UserName);
+
+                    var describeTagsParams = {
+                        ResourceId: data.Workspaces[i].WorkspaceId /* required */
+                    };
+                    workspaces.describeTags(describeTagsParams, function (err, data) {
+                        if (err) {
+                            console.log(err, err.stack);
+                        } else {
+                            //console.log(data);
+
+                            for (var i = 0; i < data.TagList.length; i++) {
+                                if (data.TagList[i].Key == "SelfServiceManaged" && data.TagList[i].Value == event.requestContext.authorizer.claims.email) {
+                                    console.log("Desktop for '" + event.requestContext.authorizer.claims.email + "' found: " + describeTagsParams.ResourceId);
+                                    console.log("Rebuilding desktop '" + describeTagsParams.ResourceId + " per request.");
+
+                                    var rebuildParams = {
+                                        RebuildWorkspaceRequests: [{
+                                            WorkspaceId: describeTagsParams.ResourceId
+                                        }]
+                                    };
+
+                                    console.log(JSON.stringify(rebuildParams));
+
+                                    workspaces.rebuildWorkspaces(rebuildParams, function (err, data) {
+                                        if (err) {
+                                            console.log("Error: " + err);
+                                            callback(null, {
+                                                statusCode: 500,
+                                                body: JSON.stringify({
+                                                    Error: err,
+                                                }),
+                                                headers: {
+                                                    'Access-Control-Allow-Origin': '*',
+                                                },
+                                            });
+                                        } else {
+                                            // TODO: Check if "FailedRequests" is empty.
+                                            console.log("Result: " + JSON.stringify(data));
+                                            
+                                            callback(null, {
+                                                "statusCode": 200,
+                                                "body": JSON.stringify({
+                                                    Result: data
+                                                }),
+                                                "headers": {
+                                                    "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                                                    "Access-Control-Allow-Methods": "GET,OPTIONS",
+                                                    "Access-Control-Allow-Origin": originURL
+                                                }
+                                            });
+                                        }
+                                    });
+
+                                }
+                            }
+
+                        }
+                    });
+                }
+
+            }
+        });
+
+    } else if (action == "reboot") {
+        console.log("Trying to find desktop owned by: " + event.requestContext.authorizer.claims.email);
+
+        var describeWorkspacesParams = [];
+
+        workspaces.describeWorkspaces(describeWorkspacesParams, function (err, data) {
+            if (err) {
+                console.log(err, err.stack); // an error occurred
+            } else {
+                //console.log(data);
+                //console.log(data.Workspaces[0].WorkspaceId);
+
+                for (var i = 0; i < data.Workspaces.length; i++) {
+                    //console.log("Desktop[" + i + "]: " + data.Workspaces[i].WorkspaceId + " is owned by: " + data.Workspaces[i].UserName);
+
+                    var describeTagsParams = {
+                        ResourceId: data.Workspaces[i].WorkspaceId /* required */
+                    };
+                    workspaces.describeTags(describeTagsParams, function (err, data) {
+                        if (err) {
+                            console.log(err, err.stack);
+                        } else {
+                            //console.log(data);
+
+                            for (var i = 0; i < data.TagList.length; i++) {
+                                if (data.TagList[i].Key == "SelfServiceManaged" && data.TagList[i].Value == event.requestContext.authorizer.claims.email) {
+                                    console.log("Desktop for '" + event.requestContext.authorizer.claims.email + "' found: " + describeTagsParams.ResourceId);
+                                    console.log("Rebooting desktop '" + describeTagsParams.ResourceId + " per request.");
+
+                                    var rebootParams = {
+                                        RebootWorkspaceRequests: [{
+                                            WorkspaceId: describeTagsParams.ResourceId
+                                        }]
+                                    };
+
+                                    console.log(JSON.stringify(rebootParams));
+
+                                    workspaces.rebootWorkspaces(rebootParams, function (err, data) {
+                                        if (err) {
+                                            console.log("Error: " + err);
+                                            callback(null, {
+                                                statusCode: 500,
+                                                body: JSON.stringify({
+                                                    Error: err,
+                                                }),
+                                                headers: {
+                                                    'Access-Control-Allow-Origin': '*',
+                                                },
+                                            });
+                                        } else {
+                                            // TODO: Check if "FailedRequests" is empty.
+                                            console.log("Result: " + JSON.stringify(data));
+                                            
+                                            callback(null, {
+                                                "statusCode": 200,
+                                                "body": JSON.stringify({
+                                                    Result: data
+                                                }),
+                                                "headers": {
+                                                    "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+                                                    "Access-Control-Allow-Methods": "GET,OPTIONS",
+                                                    "Access-Control-Allow-Origin": originURL
+                                                }
+                                            });
+                                        }
+                                    });
+
+                                }
+                            }
+
+                        }
+                    });
+                }
+
+            }
+        });
+
     } else if (action == "delete") {
         console.log("Trying to find desktop owned by: " + event.requestContext.authorizer.claims.email);
 
--- website/js/index.js
+++ website/js/index.js
@@ -19,11 +19,19 @@ var authToken;
     });
 
     $("#methodStatus").hide();
+    $("#DesktopNoExist").hide();
+    $("#DesktopExist").hide();
 
     $(function onDocReady() {
 
         $('#RequestWorkSpace').submit(handleRequest);
         $('#DecommissionWorkSpace').submit(handleDecommission);
+        $('#RebootWorkSpace').submit(handleReboot);
+        $('#RebuildWorkSpace').submit(handleRebuild);
+
+        $("#reloadButton").on('click', function() {
+            location.reload();
+        });
 
     });
 
@@ -79,6 +87,84 @@ var authToken;
 
     }
 
+    function handleReboot(event) {
+        event.preventDefault();
+
+        $.ajax({
+            method: 'POST',
+            url: WORKSPACES_CONTROL_URL,
+            headers: {
+                Authorization: authToken
+            },
+            beforeSend: function () {
+                //$('#loading-image').show();
+                console.log("json: " + JSON.stringify({
+                    action: 'reboot'
+                }));
+            },
+            complete: function () {
+                //$('#loading-image').hide();
+            },
+            data: JSON.stringify({
+                action: 'reboot'
+            }),
+            contentType: 'text/plain',
+            error: function () {
+                $("#methodStatus").addClass("alert-danger");
+                $("#methodStatus").html("<b>Error! Something went wrong... Please contact an administrator if the problem persists.")
+                $("#methodStatus").show();
+            },
+            success: function (data) {
+                $("#methodStatus").addClass("alert-success");
+                $("#methodStatus").html("<b>Success! WorkSpace reboot in-progress...</b> Please allow up to 5 minutes for the virtual desktop to be fully rebooted.")
+                $("#methodStatus").show();
+                setTimeout(function () {
+                    location.reload();
+                }, 60000);
+            }
+        });
+
+    }
+
+    function handleRebuild(event) {
+        event.preventDefault();
+        
+        $.ajax({
+            method: 'POST',
+            url: WORKSPACES_CONTROL_URL,
+            headers: {
+                Authorization: authToken
+            },
+            beforeSend: function () {
+                //$('#loading-image').show();
+                console.log("json: " + JSON.stringify({
+                    action: 'rebuild'
+                }));
+            },
+            complete: function () {
+                //$('#loading-image').hide();
+            },
+            data: JSON.stringify({
+                action: 'rebuild'
+            }),
+            contentType: 'text/plain',
+            error: function () {
+                $("#methodStatus").addClass("alert-danger");
+                $("#methodStatus").html("<b>Error! Something went wrong... Please contact an administrator if the problem persists.")
+                $("#methodStatus").show();
+            },
+            success: function (data) {
+                $("#methodStatus").addClass("alert-success");
+                $("#methodStatus").html("<b>Success! WorkSpace rebuild in-progress...</b> Please allow up to 10 minutes for the virtual desktop to be fully rebuilt.")
+                $("#methodStatus").show();
+                setTimeout(function () {
+                    location.reload();
+                }, 60000);
+            }
+        });
+
+    }
+
     function handleDecommission(event) {
         event.preventDefault();
 
@@ -116,6 +202,9 @@ var authToken;
                 $("#methodStatus").addClass("alert-success");
                 $("#methodStatus").html("<b>Success! WorkSpace removal in-progress...</b> Please allow up to 10 minutes for the virtual desktop to be fully removed.")
                 $("#methodStatus").show();
+                setTimeout(function () {
+                    location.reload();
+                }, 60000);
             }
         });
     }
@@ -124,6 +213,38 @@ var authToken;
         if (!_config.api.invokeUrl) {
             $('#noApiMessage').show();
         }
+
+        $.ajax({
+            method: 'POST',
+            url: WORKSPACES_CONTROL_URL,
+            headers: {
+                Authorization: authToken
+            },
+            beforeSend: function () {
+                //$('#loading-image').show();
+                console.log("json: " + JSON.stringify({
+                    action: 'list'
+                }));
+            },
+            complete: function () {
+                //$('#loading-image').hide();
+            },
+            data: JSON.stringify({
+                action: 'list'
+            }),
+            contentType: 'text/plain',
+            error: function () {
+
+                $("#DesktopNoExist").show();
+            },
+            success: function (data) {
+                $("#workspace-Id").html(data.WorkspaceId);
+                $("#workspace-Username").html(data.UserName);
+                $("#workspace-State").html(data.State);
+                $("#workspace-Bundle").html(data.BundleId);
+                $("#DesktopExist").show();
+            }
+        });
     });
 
 }(jQuery));
