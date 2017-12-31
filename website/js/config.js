6082520cffb352c8dc3dd3acb741e5c47fdccc9c
Earl Gay <earl@eeg3.net>
2017-12-30 21:17:56 -0500

    Updated config for us-west-2

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_fzvmbiCHh', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5du7qi995aacqiovighd34qd80', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_tO8QHyi7E', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1qp0ovmeoqa6dg0388onirait5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://veutzq4rwl.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://gmvj8sbjjk.execute-api.us-west-2.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'earl@eeg3.net'
