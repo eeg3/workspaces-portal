7cfb397ce10bfc8ac6215ebbc8f5d09f4a69b415
Earl Gay <earl@eeg3.net>
2017-12-31 21:37:52 -0500

    Initial workspaces-details

--- lambda/workspaces-details/workspaces-details.js
+++ lambda/workspaces-details/workspaces-details.js
@@ -12,7 +12,7 @@ exports.handler = (event, context, callback) => {
 
     var originURL = process.env.ORIGIN_URL || '*';
 
-    var tableName = process.env.DETAILS_TABLE_NAME || 'wsPortal-us-west-2-workspaces';
+    var tableName = process.env.DETAILS_TABLE_NAME || 'wsp-db-int-serverless-stack-WorkspaceDetailsTable-WTRSGHNEH36H';
 
     console.log('Received event:', JSON.stringify(event, null, 2));
 
@@ -21,16 +21,16 @@ exports.handler = (event, context, callback) => {
     var action = JSON.parse(event.body)["action"];
 
     if (action == "get") {
-        /*
-        var opp = JSON.parse(event.body)["opp"];
+
+        var user = JSON.parse(event.body)["user"];
         var params = {
             TableName: tableName,
             Key: {
-                'OPPID': {
-                    S: opp
+                'Username': {
+                    S: user
                 },
             },
-            ProjectionExpression: 'NOTES'
+            ProjectionExpression: 'Username,Email,Status'
         };
 
         // Call DynamoDB to read the item from the table
@@ -47,11 +47,13 @@ exports.handler = (event, context, callback) => {
                     },
                 });
             } else if (data.Item) {
-                console.log("Value: " + data.Item.NOTES.S);
+                console.log("Username: " + data.Item.Username.S);
+                console.log("Email: " + data.Item.Email.S)
+                console.log("Status: " + data.Item.Status.S)
                 callback(null, {
                     "statusCode": 200,
                     "body": JSON.stringify({
-                        Note: data.Item.NOTES.S
+                        Result: JSON.stringify(data.Item)
                     }),
                     "headers": {
                         "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
@@ -63,7 +65,7 @@ exports.handler = (event, context, callback) => {
                 callback(null, {
                     "statusCode": 200,
                     "body": JSON.stringify({
-                        Note: "E_NO_NOTE"
+                        Note: "E_NOT_FOUND"
                     }),
                     "headers": {
                         "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
@@ -73,19 +75,23 @@ exports.handler = (event, context, callback) => {
                 });
             }
         });
-        */
+
     } else if (action == "put") {
-        /*
-        var opp = JSON.parse(event.body)["opp"];
-        var note = JSON.parse(event.body)["note"];
+
+        var user = JSON.parse(event.body)["user"];
+        var email = JSON.parse(event.body)["email"];
+        var status = JSON.parse(event.body)["status"];
         var params = {
             TableName: tableName,
             Item: {
-                'OPPID': {
-                    S: opp
+                'Username': {
+                    S: user
                 },
-                'NOTES': {
-                    S: note
+                'Email': {
+                    S: email
+                },
+                'Status': {
+                    S: status
                 }
             }
         };
@@ -120,7 +126,7 @@ exports.handler = (event, context, callback) => {
                 });
             }
         });
-        */
+
     }
 
 
