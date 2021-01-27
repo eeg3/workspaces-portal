252e19d46675408e8935a6cbe4204d836acb6419
sameeramahajan <sameera.mahajan.85@gmail.com>
2021-01-27 16:15:21 +0530

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -2,12 +2,12 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_ABCDEFG', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1a2b3c4d5f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://abcdefghijkl.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
-        email: 'earl@eeg3.net'
+        email: 'vgadmin@vanguard-infosys.com'
     }
 };
