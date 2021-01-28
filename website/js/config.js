0fcc39c13979a45203b1da8c8fead087b6262aa2
sameeramahajan <sameera.mahajan.85@gmail.com>
2021-01-28 14:18:37 +0530

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,11 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ABCDEFG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1a2b3c4d5f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_KkExZU8pa', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'qlsi6oha329cdve747954gnuc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-southeast-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://abcdefghijkl.execute-api.us-east-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://efjhhnqitd.execute-api.ap-southeast-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     },
     approval: {
         email: 'vgadmin@vanguard-infosys.com'
